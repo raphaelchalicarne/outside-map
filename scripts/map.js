ad51d29123eb4a9de432fb77daa30ce8d3e71b3b
Raphaël Chalicarne <raphael.chalicarne@foodyssey.co>
2022-11-13 12:33:18 +0100

    :wrench: Set default position & zoom

--- scripts/map.js
+++ scripts/map.js
@@ -2,6 +2,6 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicmFwaDU5IiwiYSI6ImNsYWY3bHRxcTA4dTEzeG83ZjB2b
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/examples/cke97f49z5rlg19l310b7uu7j', // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-  center: [41, 21], // starting position
-  zoom: 3 // starting zoom
+  center: [4.840, 45.755], // starting position
+  zoom: 11 // starting zoom
 });
