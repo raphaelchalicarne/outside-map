48de29d32eb7f6d6c81671cdf4329446f1051180
Raphaël Chalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2022-11-13 14:11:30 +0100

    :art: Organise index.html files (#2)
    * :lipstick: Add style file
    
    * :art: Import map in js file
    
    * :wrench: Set default position & zoom
    
    * :speech_balloon: Update title
    
    * :lipstick: Use custom Mapbox style
    
    * :see_no_evil: Add .gitignore file
    
    * :truck: Rename scripts folder
    
    * :closed_lock_with_key: Store private key
    
    * :construction_worker: Add CD workflow

---
+++ js/map.js
@@ -0,0 +1,9 @@
+import MAPBOX_PK from './config.js';
+
+mapboxgl.accessToken = MAPBOX_PK;
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/raph59/claf81h0300bn15n7hevdbfl1', // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+  center: [4.840, 45.755], // starting position
+  zoom: 11 // starting zoom
+});
