21d48fb30d6d1d87db8d0d55bfe485e99d9a22e0
Raphaël Chalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2022-11-13 15:25:14 +0100

    :closed_lock_with_key: Can't hide secret token (#3)
    * :closed_lock_with_key: Can't hide secret token
    
    * :fire: Delete useless import

--- js/map.js
+++ js/map.js
@@ -1,6 +1,4 @@
-import MAPBOX_PK from './config.js';
-
-mapboxgl.accessToken = MAPBOX_PK;
+mapboxgl.accessToken = 'pk.eyJ1IjoicmFwaDU5IiwiYSI6ImNsYWY3bHRxcTA4dTEzeG83ZjB2bzkwYmUifQ.Ye2p9OAgykrdKe0fLWSN7A';
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/raph59/claf81h0300bn15n7hevdbfl1', // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
