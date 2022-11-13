764dd95e17fcbac069ff1e0a621cc24bae9ce08a
Raphaël Chalicarne <raphael.chalicarne@foodyssey.co>
2022-11-13 13:10:48 +0100

    :closed_lock_with_key: Store private key

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
-mapboxgl.accessToken = ENV['MAPBOX_PK'];
+import MAPBOX_PK from './config.js';
+
+mapboxgl.accessToken = MAPBOX_PK;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/raph59/claf81h0300bn15n7hevdbfl1', // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
