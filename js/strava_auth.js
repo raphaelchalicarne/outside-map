14fe0b2ce0d16728d21845b08c6a7d0b956b4826
Raphaël Chalicarne <raphael.chalicarne@foodyssey.co>
2022-11-15 19:59:52 +0100

    :art: Do not call Strava API when unauthenticated

--- js/strava_auth.js
+++ js/strava_auth.js
@@ -6,7 +6,7 @@ function stravaAuth() {
   var auth_url = new URL(STRAVA_OAUTH);
   auth_url.searchParams.set('client_id', CLIENT_ID);
   auth_url.searchParams.set('response_type', 'code');
-  auth_url.searchParams.set('redirect_uri', window.location.href);
+  auth_url.searchParams.set('redirect_uri', new URL(window.location.href).origin);
   auth_url.searchParams.set('approval_prompt', 'force');
   auth_url.searchParams.set('scope', 'read,activity:read');
   window.open(auth_url);
@@ -14,28 +14,32 @@ function stravaAuth() {
 
 document.addEventListener('DOMContentLoaded', async function () {
   let params = (new URL(document.location)).searchParams;
-  let code = params.get('code');
-  let data = {
-    'client_id': CLIENT_ID,
-    'client_secret': '4a3241e7324cdae3408a6028cd70dab9994ef8c1',
-    'code': code,
-    'grant_type': 'authorization_code'
-  };
+  var code = params.get('code');
+  if (code === null) {
+    return;
+  } else {
+    let data = {
+      'client_id': CLIENT_ID,
+      'client_secret': '4a3241e7324cdae3408a6028cd70dab9994ef8c1',
+      'code': code,
+      'grant_type': 'authorization_code'
+    };
 
-  var headers = new Headers();
-  headers.append('Accept', 'application/json');
-  headers.append('Content-Type', 'application/json');
+    var headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
 
-  var requestOptions = {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(data)
-  };
+    var requestOptions = {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data)
+    };
 
-  const response = await fetch('https://www.strava.com/oauth/token', requestOptions)
-    .then((response) => response.json())
-    .then((data) => LogSuccess(data))
-    .catch((error) => console.error('Error:', error));
+    const response = await fetch('https://www.strava.com/oauth/token', requestOptions)
+      .then((response) => response.json())
+      .then((data) => LogSuccess(data))
+      .catch((error) => console.error('Error:', error));
+  }
 }, false);
 
 async function LogSuccess(data) {
@@ -51,6 +55,7 @@ async function fetch_activities(athlete_data) {
 
   var requestOptions = {
     method: 'GET',
+    cache: 'no-cache',
     headers: headers,
     redirect: 'follow'
   };
